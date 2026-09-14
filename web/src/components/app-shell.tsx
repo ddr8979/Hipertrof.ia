fedcf2aa763cb9a1f0fb922f4309c0ecdf5c845a
ddr8979 <ddr8979@users.noreply.github.com>
2026-09-14 00:42:10 -0300

    fix: inline Spotify SVG in app-shell + use SpotifyIcon in dashboard

--- web/src/components/app-shell.tsx
+++ web/src/components/app-shell.tsx
@@ -50,8 +50,20 @@ function NowPlayingMini() {
           className="size-8 shrink-0 rounded-lg object-cover"
         />
       ) : (
+        // eslint-disable-next-line @next/next/no-inline-styles
         <span className="flex size-8 shrink-0 items-center justify-center rounded-lg bg-[#1DB954]/15 text-[#1DB954]">
-          <SpotifyIcon size={16} />
+          <svg
+            role="img"
+            viewBox="0 0 24 24"
+            width={16}
+            height={16}
+            aria-label="Spotify"
+            fill="#1ED760"
+            dangerouslySetInnerHTML={{
+              __html:
+                '<path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.55 0 12 0zm5.5 17.33c-.24.36-.66.48-1.02.24-2.82-1.74-6.36-2.1-10.56-1.14-.42.12-.78-.18-.9-.54-.12-.42.18-.78.54-.9 4.56-1.02 8.52-.6 11.64 1.32.42.18.48.66.3 1.02zm1.45-3.3c-.3.42-.84.6-1.26.3-3.24-1.98-8.16-2.58-11.94-1.38-.48.12-1.02-.12-1.14-.6-.12-.48.12-1.02.6-1.14C9.5 9.9 15 10.56 18.72 12.84c.36.18.54.78.24 1.2zm.13-3.36C15.24 8.4 8.82 8.16 5.16 9.3c-.6.18-1.2-.18-1.38-.72-.18-.6.18-1.2.72-1.38 4.26-1.26 11.28-1.02 15.72 1.62.54.3.72 1.02.42 1.56-.3.42-1.02.6-1.56.3z"></path>',
+            }}
+          />
         </span>
       )}
       <span className="flex min-w-0 flex-col">
